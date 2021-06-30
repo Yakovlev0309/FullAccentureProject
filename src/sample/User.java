fb6fdbea6356b2f1f307c2c87dc0b209891c9b82
Садовский Олег <0.I.3.g.S.a.d@gmail.com>
2021-06-30 19:11:55 +0300

    Изменения в классе User (добавлены сеттеры и геттеры, технические методы)

--- src/sample/User.java
+++ src/sample/User.java
@@ -1,24 +1,69 @@
 package sample;
 
+import repository.Buildings;
+import repository.Database;
+import repository.Users;
+
+import javax.xml.crypto.Data;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class User {
     private String username;
     private String password;
     private String type;
     private Building building;
+    public User(){}
 
-    public User(ResultSet result) throws SQLException {
-        username = result.getString("USERNAME");
-        password = result.getString("PASSWORD");
-        building = new Building(Users.GetAssignedBuilding(result));
-        //type = ...
+    public String getUsername() {
+        return username;
     }
-    public String Type() {
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
         return type;
     }
-    public Building GetBuilding() {
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Building getBuilding() {
         return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
+
+    @Override
+    public String toString(){
+        return "User\n" +
+                username+"\n" +
+                password+"\n" +
+                type+"\n";
+    }
+    public static void main (String[] args){
+        try {
+            Database db = new Database();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        User user = Users.GetUser("store2worker","worker");
+        Building building = user.getBuilding();
+        ArrayList<Product> products = building.getProducts();
+        System.out.println(user);
     }
 }
