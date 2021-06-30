c5e25b13b6b945caab1beeec6945a06c401f7d87
Садовский Олег <0.I.3.g.S.a.d@gmail.com>
2021-06-30 18:39:44 +0300

    Изменения в классе Storage (предыдущие методы определены в родительском классе)

--- src/sample/Storage.java
+++ src/sample/Storage.java
@@ -1,18 +1,8 @@
 package sample;
 
 public class Storage extends Building {
-    @Override
-    public void AddProducts(int count) {
-        for (byte i = 0; i < count; i++) {
-            //Берём товары с завода
-        }
-    }
-    @Override
-    public void SendTo(Building building, int count) {
-        for (byte i = 0; i < count; i++) {
-            building.Products().add(Products().get(i));
-            Products().remove(0);
-            Products().get(i).SetNewLocation(building);
-        }
-    }
+
+    public static final String type = "storage";
+
+    public Storage(){super();}
 }
