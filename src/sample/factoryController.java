08423c5fb603877f51807850be41297061bc1321
Nikita <yakovlev.universe@gmail.com>
2021-07-01 00:44:51 +0300

    Changes by Oleg

--- src/sample/factoryController.java
+++ src/sample/factoryController.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class factoryController {
