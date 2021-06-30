59b3707747c7841fb02c8a3c1209fcd84e811a76
Садовский Олег <0.I.3.g.S.a.d@gmail.com>
2021-06-30 18:43:54 +0300

    Изменения в классе Product (добавлены сеттеры и геттеры, методы для удаления и задания нового расположения продукта)

--- src/sample/Product.java
+++ src/sample/Product.java
@@ -1,16 +1,31 @@
 package sample;
 
+import repository.Products;
+
 public class Product {
     //Уникальный номер для каждого продукта
     private int id;
+
+    //Метод доступа к уникальному номеру
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public Product(int id) {
         this.id = id;
     }
-    //Метод доступа к уникальному номеру
-    public int GetId() {
-        return id;
+
+    public Product(){}
+
+    public void deleteProduct(){
+        Products.deleteProduct(this);
     }
+
     public void SetNewLocation(Building building) {
-        //Products.ChangeLocation(this, building);
+        Products.changeLocation(this, building);
     }
 }
