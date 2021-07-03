041d0777ca26bf9c31df951e849facbb0ff3796b
Садовский Олег <0.I.3.g.S.a.d@gmail.com>
2021-07-03 20:00:55 +0300

    Рефакторинг Users

--- src/repository/Users.java
+++ src/repository/Users.java
@@ -1,5 +1,6 @@
 package repository;
 
+import org.jetbrains.annotations.Nullable;
 import sample.Building;
 import sample.Product;
 import sample.User;
@@ -7,6 +8,7 @@ import sample.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Users extends BaseTable{
     public static User getUser(String username, String password){
@@ -14,30 +16,7 @@ public class Users extends BaseTable{
         User user = new User();
         try {
             result = getDataSQL("SELECT * FROM USERS WHERE USERNAME = '"+username+"' AND PASSWORD = '"+password+"'");
-            result.next();
-            user = new User();
-            user.setUsername(result.getString("USERNAME"));
-            user.setPassword(result.getString("PASSWORD"));
-            {
-                int typeId = result.getInt("TYPE_ID");
-                ResultSet type = getDataSQL("SELECT * FROM USER_TYPES WHERE ID ="+typeId);
-                type.next();
-                user.setType(type.getString("TYPE_NAME" ));
-            }
-            user.setBuilding(Buildings.GetAssignedBuilding(user));
-            {
-                int shiftId = result.getInt("SHIFT_ID");
-                ResultSet shift = getDataSQL("SELECT * FROM SHIFTS WHERE ID="+shiftId);
-                shift.next();
-                try {
-                    user.setShift(shift.getString("SHIFT_NAME" ));
-                } catch (SQLException throwables) {
-                    user.setShift("");
-                }
-            }
-            user.setEfficiency(result.getDouble("EFFICIENCY"));
-            user.setName(result.getString("NAME"));
-            user.setSurname(result.getString("SURNAME"));
+            user = makeUser(result, null);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -50,26 +29,7 @@ public class Users extends BaseTable{
         try {
             result = getDataSQL("SELECT * FROM USERS WHERE BUILDING_ID = (SELECT ID FROM BUILDINGS WHERE NAME = '"+building.getName()+"')");
             while (result.next()){
-                user = new User();
-                user.setUsername(result.getString("USERNAME"));
-                user.setPassword(result.getString("PASSWORD"));
-                {
-                    //Установка типа
-                    ResultSet type = getDataSQL("SELECT * FROM USER_TYPES WHERE ID ="+result.getInt("TYPE_ID"));
-                    type.next();
-                    user.setType(type.getString("TYPE_NAME"));
-                }
-                user.setBuilding(building);
-                {
-                    //Установка смены
-                    ResultSet shift = getDataSQL("SELECT * FROM SHIFTS WHERE ID ="+result.getInt("SHIFT_ID"));
-                    shift.next();
-                    user.setShift(shift.getString("SHIFT_NAME"));
-                }
-                user.setEfficiency(result.getDouble("EFFICIENCY"));
-                user.setName(result.getString("NAME"));
-                user.setSurname(result.getString("SURNAME"));
-                users.add(user);
+                users.add(makeUser(result, building));
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -83,6 +43,39 @@ public class Users extends BaseTable{
             result = getDataSQL("SELECT NAME, SURNAME FROM USERS WHERE ID = (SELECT USER_ID FROM PRODUCTS WHERE ID="+product.getId()+")");
             result.next();
             user = result.getString("NAME")+" "+result.getString("SURNAME");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return user;
+    }
+    static User makeUser(ResultSet result, @Nullable Building building){
+        User user = new User();
+        try {
+            if(result.isBeforeFirst()){
+                result.next();
+            }
+            user.setUsername(result.getString("USERNAME"));
+            user.setPassword(result.getString("PASSWORD"));
+            {
+                int typeId = result.getInt("TYPE_ID");
+                ResultSet type = getDataSQL("SELECT * FROM USER_TYPES WHERE ID ="+typeId);
+                type.next();
+                user.setType(type.getString("TYPE_NAME" ));
+            }
+            user.setBuilding(Objects.requireNonNullElseGet(building, () -> Buildings.GetAssignedBuilding(user)));
+            {
+                int shiftId = result.getInt("SHIFT_ID");
+                ResultSet shift = getDataSQL("SELECT * FROM SHIFTS WHERE ID="+shiftId);
+                shift.next();
+                try {
+                    user.setShift(shift.getString("SHIFT_NAME" ));
+                } catch (SQLException throwables) {
+                    user.setShift("");
+                }
+            }
+            user.setEfficiency(result.getDouble("EFFICIENCY"));
+            user.setName(result.getString("NAME"));
+            user.setSurname(result.getString("SURNAME"));
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
